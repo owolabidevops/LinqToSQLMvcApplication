f673436e1c29cca7fe4a4abb7f3e44b7462bed3b
owolabi <babsdgreater@gmail.com>
2020-09-24 10:43:27 +0100

    first commit

---
+++ LinqToSQLMvcApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinqToSQLMvcApplication.Startup))]
+namespace LinqToSQLMvcApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
